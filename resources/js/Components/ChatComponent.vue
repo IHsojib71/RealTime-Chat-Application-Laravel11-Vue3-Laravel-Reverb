8ce905762ccff46787d952e4821e84d336a468ae
Md Ismail Hossain <ihsojib43@gmail.com>
2024-08-28 21:24:32 +0600

    added typing event

--- resources/js/Components/ChatComponent.vue
+++ resources/js/Components/ChatComponent.vue
@@ -4,7 +4,7 @@ import { ref, onMounted, defineProps, watch, nextTick } from 'vue';
 import { usePage } from '@inertiajs/vue3';
 
 const messages = ref();
-
+const userTyping = ref(false);
 const currentUser = usePage().props.auth.user;
 
 const props = defineProps({
@@ -24,11 +24,20 @@ watch(messages, () => {
         behavior : "smooth",
       })
     })
-    messageContainer.value.scrollTo({
-        top: messageContainer.value.scrollHeight,
-        behavior : "smooth",
-    })
 }, { deep: true });
+watch(userTyping, () => {
+    nextTick(() => {
+        if (userTyping.value === true)
+        {
+            messageContainer.value.scrollTo({
+            top: messageContainer.value.scrollHeight,
+            behavior : "smooth",
+      })
+        }
+
+    })
+
+});
 
 const newMessage = ref('');
 
@@ -49,6 +58,13 @@ const sendMessage = () => {
     }
 }
 
+const sendTypingEvent = () => {
+    Echo.private(`chat.${props.friend.id}`)
+        .whisper("typing", {
+            user_id: currentUser.id
+        });
+}
+
 onMounted(() => {
     axios.get(route('chat.fetchMessages', props.friend.id))
         .then((response) => {
@@ -57,10 +73,18 @@ onMounted(() => {
         .catch((error) => {
             console.log(error);
         })
-    Echo.private(`chat.${currentUser.id}`)
+
+
+      Echo.private(`chat.${currentUser.id}`)
         .listen('MessageSentEvent', (response) => {
             messages.value.push(response.message);
         })
+        .listenForWhisper("typing", (response) => {
+            userTyping.value = response.user_id === props.friend.id;
+            setTimeout(() => {
+                userTyping.value = false;
+              },1500)
+          })
 })
 </script>
 
@@ -73,8 +97,9 @@ onMounted(() => {
         </div>
       </div>
     </div>
+    <span v-if="userTyping" class="text-red-200 font-bold text-xs mt-4 rounded-3xl  animate-pulse">{{props.friend.name}} is typing . . . . . .</span>
     <div class="flex items-center mt-4">
-      <input v-model="newMessage" type="text" class="flex-grow px-4 py-2 rounded-l-lg" placeholder="Type your message...">
+      <input @keydown="sendTypingEvent" v-model="newMessage" type="text" class="flex-grow px-4 py-2 rounded-l-lg" placeholder="Type your message...">
       <button @click="sendMessage" class="px-4 py-2 rounded-r-lg bg-blue-500 text-white">Send</button>
     </div>
   </div>
